87bf440b32a91619c357ee6d25d5ff4f34ee4f2f
Melany Salas F <dhnmelfer@estudiantec.cr>
2023-10-06 19:27:53 -0600

    Continuando trabajo con backend

---
+++ Backend/src/app.ts
@@ -0,0 +1,6 @@
+//Aquí va la configuración de express
+import express from "express";
+
+const app = express()
+
+export default app;
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,6 @@
-console.log("hello word");
+import app from "./app"
+
+app.listen(3000, () => {
+    console.log("probando el servidor");
+})
+
