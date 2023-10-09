cb8ede32a5127e9e19a7fa76107ae4ca0ecf9ac9
Melany Salas F <dhnmelfer@estudiantec.cr>
2023-10-08 23:50:40 -0600

    Update Image_.ts

--- Backend/src/Model/Image_.ts
+++ Backend/src/Model/Image_.ts
@@ -5,7 +5,7 @@ export class Image_ {
     private id : Number = 0;
     private height : Number = 0;
     private width : Number = 0;
-    imageType: ImageType = 0;
+    private imageType: ImageType = 0;
 
     public constructor(id : Number, height : Number, width : Number, imageType: ImageType){
         this.id = id;
