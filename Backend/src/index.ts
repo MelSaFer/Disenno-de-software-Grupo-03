7befdafc4c77db1d26058a34bbb37c442ed321d1
Melany Salas F <dhnmelfer@estudiantec.cr>
2023-10-08 22:52:01 -0600

    Updates en DAO

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,52 +1,52 @@
-import app from "./app"
+import app from "./app";
 import { SingletonFirebase } from "./Controler/Singleton/SingletonFirebase";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { SingletonMongo } from "./Controler/Singleton/SingletonMongo";
-import {DATABASE_NAME, PRODUCT_COLLECTION, PORT} from './Controler/Singleton/config';
+import {
+  DATABASE_NAME,
+  PRODUCT_COLLECTION,
+  PORT,
+} from "./Controler/Singleton/config";
 
-require('dotenv').config();
+require("dotenv").config();
 
 async function main() {
-    //await connectDB();
-    //await connectDB();
-    SingletonMongo.getInstance().connect();
-	const db = SingletonMongo.getInstance().getDatabase(DATABASE_NAME);
-	const collection = db.collection(PRODUCT_COLLECTION);
-	const doc = { name: "Bing", type: "search engine" };
-	collection.insertOne(doc);
-    //const singletonMongo = SingletonMongo.getInstance();
-    const singletonFirebase = SingletonFirebase.getInstance();
-    //const connection = await SingletonMongo;
+  //await connectDB();
+  //await connectDB();
+  SingletonMongo.getInstance().connect();
+  const db = SingletonMongo.getInstance().getDatabase(DATABASE_NAME);
+  const collection = db.collection(PRODUCT_COLLECTION);
+  const doc = { name: "Bing", type: "search engine" };
+  collection.insertOne(doc);
+  //const singletonMongo = SingletonMongo.getInstance();
+  const singletonFirebase = SingletonFirebase.getInstance();
+  //const connection = await SingletonMongo;
 
-	
-
-    if (singletonFirebase instanceof SingletonFirebase) {
-      if (singletonFirebase){
-
-        //obtener credenciales solo para ver si funciona esta cosa
-        const auth = getAuth(singletonFirebase.getApp());
-        signInWithEmailAndPassword(auth, "fio@gmail.com", "fio123")
-          .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            console.log(user.email);
-            // ...
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-          });
-
-      } else{
-        console.log("2")
-      }
+  if (singletonFirebase instanceof SingletonFirebase) {
+    if (singletonFirebase) {
+      //obtener credenciales solo para ver si funciona esta cosa
+      const auth = getAuth(singletonFirebase.getApp());
+      signInWithEmailAndPassword(auth, "fio@gmail.com", "fio123")
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user.email);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    } else {
+      console.log("2");
     }
-
-    app.listen(PORT);
-    console.log("Server on port ", PORT);
   }
-  
-  main();
+
+  app.listen(PORT);
+  console.log("Server on port ", PORT);
+}
+
+main();
 
 // app.use("/login", (require("./routes/loginRoutes")))
 // app.use("/register", (require("./routes/userRoutes")))
