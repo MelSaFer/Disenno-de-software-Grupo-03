3fc41f32ad75c59b615325d5f3a060108cf63ec7
fiozelaya <fiorella.zelaya1024@estudiantec.cr>
2023-10-23 17:42:27 -0600

    deleting ids

--- Backend/src/Controler/DAO/schemas/Schemas.ts
+++ Backend/src/Controler/DAO/schemas/Schemas.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 //USER SCHEMA------------------------------------------------------------------------------------------------------------------------------
 export const UserSchema = new mongoose.Schema({
-    userId: { type: Number, required: true },
+    userId: { type: String, required: true },
     roleType: { type: String, required: true },
     cart: { type: Array, required: true }
 });
@@ -11,13 +11,13 @@ export const UserSchema = new mongoose.Schema({
 
 //CART SCHEMA------------------------------------------------------------------------------------------------------------------------------
 export const CartItemSchema = new mongoose.Schema({
-    productId: { type: Number, required: true },
+    productId: { type: Number, required: true }, //!!
     quantity: { type: Number, required: true }
 });
 
 //PRODUCT SCHEMA------------------------------------------------------------------------------------------------------------------------------
 export const ProductSchema = new mongoose.Schema({
-    productId: { type: Number, required: true },
+    //productId: { type: Number, required: true },
     description: { type: String, required: true },
     cuantityAvailable: { type: Number, required: true },
     imageId: { type: String, required: true },
@@ -27,7 +27,7 @@ export const ProductSchema = new mongoose.Schema({
 
 //PURCHASE HISTORY SCHEMA-------------------------------------------------------------------------------------------------------------------
 export const PurchaseSchema = new mongoose.Schema({
-    purchaseId: { type: Number, required: true },
+    //purchaseId: { type: Number, required: true },
     purchaseDetails: { type: String, required: true },
     products: { type: Array, require: true},
     voucherId: { type: String, require: true},
@@ -40,7 +40,7 @@ export const PurchaseSchema = new mongoose.Schema({
 
 //CONTENT SCHEMA------------------------------------------------------------------------------------------------------------------------------
 export const ContentSchema = new mongoose.Schema({
-    contentId: { type: Number, required: true },
+    //contentId: { type: Number, required: true },
     title: { type: String, required: true },
     description : { type: String, required: true },
     date: { type: Date, required: true },
@@ -51,14 +51,14 @@ export const ContentSchema = new mongoose.Schema({
 
 //CATEGORY SCHEMA------------------------------------------------------------------------------------------------------------------------------
 export const CategorySchema = new mongoose.Schema({
-    categoryId: { type: Number, required: true },
+    //categoryId: { type: Number, required: true },
     name: { type: String, required: true },
     subcategories: { type: Array, required: true }
 });
 
 //CATEGORY SCHEMA------------------------------------------------------------------------------------------------------------------------------
 export const SubcategorySchema = new mongoose.Schema({
-    subcategoryId: { type: Number, required: true },
+    //subcategoryId: { type: Number, required: true },
     name: { type: String, required: true },
 });
 
