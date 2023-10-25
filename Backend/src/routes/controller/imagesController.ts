e07b5cd6d389771c5805c92edb1adae808c76b91
Melany Salas F <dhnmelfer@estudiantec.cr>
2023-10-25 13:40:58 -0600

    Revert "working on uploading images"
    This reverts commit f5bd28a5b416785a3511924ed76be3b80c3ced68.

--- Backend/src/routes/controller/imagesController.ts
+++
@@ -1,8 +0,0 @@
-import e, { RequestHandler } from "express";
-import { MainController } from "../../Controler/Administradores/MainController";
-
-
-export const uploadImage: RequestHandler = async (req, res) => {
-    const body = req.body;
-    res.status(200).json("Subir imagen")
-}
--- Backend/src/routes/imagesRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-import * as imagesController from "./controller/imagesController"
-
-router.post("/uploadImage", imagesController.uploadImage);
-
-export default router;
