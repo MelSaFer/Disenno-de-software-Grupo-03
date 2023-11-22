b558e7b617240aa0ad040c6f8c03cb085e7d6bd6
Moises Solano <90010406+moisose@users.noreply.github.com>
2023-11-22 15:04:52 -0600

    history admin status changed

--- Frontend/tienda-duende/src/app/adminView/historyAdmin/page.tsx
+++ Frontend/tienda-duende/src/app/adminView/historyAdmin/page.tsx
@@ -172,7 +172,7 @@ const HistoryAdmin = () => {
                       </button>
                       <button
                         className="bg-red-500 text-white p-2 border rounded-full w-[200px] hover:bg-red-400"
-                        onClick={() => handleStatusChange(item, "DECLINED")}
+                        onClick={() => handleStatusChange(item, "REJECTED")}
                       >
                         Cancelar pedido
                       </button>
@@ -197,7 +197,7 @@ const HistoryAdmin = () => {
                   </div>
                 ) : null}
                 {/* DECLINED CASE */}
-                {item.state === "DECLINED" ? (
+                {item.state === "REJECTED" ? (
                   <div className="w-1/2 text-right pr-10">
                     <h1 className="text-red-400 font-semibold text-2xl pb-10">
                       Estado: {item.state}
